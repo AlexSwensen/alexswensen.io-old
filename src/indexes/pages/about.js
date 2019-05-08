5ec2b7884c796a949181d6614f47f80ce5b5a512
Alexander Swensen <alex.swensen@gmail.com>
2019-05-08 13:37:52 -0500

    add fav-icon

--- src/indexes/pages/about.js
+++ src/indexes/pages/about.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "../../components/layout"
+import {graphql} from 'gatsby'
 
 class AboutPage extends React.Component {
   render() {
@@ -9,7 +10,7 @@ class AboutPage extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <h1>About</h1>
         <p>Hey there! My name is Alexander Swensen!</p>
-        <p>I work as a software engineer at StudioNow.</p>
+        <p>I work as a software engineer at <a href="https://corp.studionow.com/" target="_blank" rel="noopener noreferrer">StudioNow</a>.</p>
       </Layout>
     )
   }
