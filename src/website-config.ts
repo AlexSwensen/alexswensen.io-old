4006663ee68dc5996504a3acdc3cca969f6b2233
Alexander Swensen <alex.swensen@gmail.com>
2021-03-16 15:17:27 -0500

    Update website-config.ts

--- src/website-config.ts
+++ src/website-config.ts
@@ -60,7 +60,7 @@ const config: WebsiteConfig = {
   lang: 'en',
   siteUrl: 'https://alexswensen.io',
   facebook: 'https://www.facebook.com/alexswensen',
-  twitter: 'https://twitter.com/agswens',
+  twitter: 'https://twitter.com/_alexswensen',
   github: 'https://github.com/alexswensen',
   showSubscribe: false,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
